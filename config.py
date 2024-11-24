0cce42e9a85313a3a471a16551a2f8a5be4a3733
Zeta-05 <vjimmy276@gmail.com>
2024-11-24 15:57:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '7572943683:AAGwAuHBAnnj09s8zHiBB8aczl3CqIjBTis' # Get Your Telegram Bot Token from @BotFather
+TOKEN = '7572943683:AAGXq8BK4DEKHMyIRvtFt3WgBtXcRLwyvrg' # Get Your Telegram Bot Token from @BotFather
 OWNER_ID = '7678359785' # Add Here your telegram id 
